978d8c109df06644888a496d29bda4153f9771ef
William Kowalski <williamkowalski@gmail.com>
2024-02-16 06:56:03 -0400

    Add files via upload

---
+++ stop-divi-flash.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+var elm=document.getElementsByTagName("html")[0];
+elm.style.display="none";
+document.addEventListener("DOMContentLoaded",function(event) {elm.style.display="block"});
+</script>
